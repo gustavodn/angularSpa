c74171fbf3e7d322ad9bf5b418a2eaa5354933d9
gustavo <gustavo4581@gmail.com>
2019-12-04 13:57:40 -0400

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gednapp'));
+app.use(static(__dirname + '/dist/gednapp'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/gednapp/index.html'));
+    res.sendFile(join(__dirname + '/dist/gednapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
