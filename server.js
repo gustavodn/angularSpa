cd793efad2cf2b2f671a3d0c60285ef6430b71bb
gustavo <gustavo4581@gmail.com>
2019-12-04 14:32:08 -0400

    update

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/testAngular'));
-
-app.get('/*', function(req, res) {
-
-    res.sendFile(join(__dirname + '/dist/testAngular/index.html'));
+app.use(express.static(__dirname + '/dist/testAngular'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/testAngular/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
